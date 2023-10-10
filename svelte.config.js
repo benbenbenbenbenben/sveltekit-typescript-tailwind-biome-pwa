9f72b00dc57903e2eb3981162f4ba5902b41dafe
Benjamin Babik <ben@curlyben.com>
2023-10-10 16:05:49 +0000

    biome partial...

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,12 +13,12 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		files: {
-			serviceWorker: 'src/sw.ts'
+			serviceWorker: "src/sw.ts",
 		},
 		serviceWorker: {
-			register: false
-		}
-	}
+			register: false,
+		},
+	},
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,56 +1,57 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit'
-import { defineConfig } from 'vitest/config';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { SvelteKitPWA } from "@vite-pwa/sveltekit";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	server: {
-		host: true
+		host: true,
 	},
 	define: {
-		'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+		"process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
 	},
 	plugins: [
 		sveltekit(),
 		SvelteKitPWA({
-			strategies: 'injectManifest',
+			strategies: "injectManifest",
 			workbox: {
-				mode: process.env.NODE_ENV ? 'production' : 'development',
+				mode: process.env.NODE_ENV ? "production" : "development",
 			},
 			devOptions: {
-				enabled: true, 
-				type: "module"
+				enabled: true,
+				type: "module",
 			},
-			srcDir: 'src',
-			filename: 'sw.ts',
+			srcDir: "src",
+			filename: "sw.ts",
 			manifest: {
-				short_name: 'SvelteKit PWA',
-				name: 'SvelteKit PWA',
-				start_url: '/',
-				scope: '/',
-				display: 'standalone',
+				short_name: "SvelteKit PWA",
+				name: "SvelteKit PWA",
+				start_url: "/",
+				scope: "/",
+				display: "standalone",
 				theme_color: "#ffffff",
 				background_color: "#ffffff",
 				icons: [
 					{
-						src: '/pwa-192x192.png',
-						sizes: '192x192',
-						type: 'image/png',
+						src: "/pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
 					},
 					{
-						src: '/pwa-512x512.png',
-						sizes: '512x512',
-						type: 'image/png',
+						src: "/pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
 					},
 					{
-						src: '/pwa-512x512.png',
-						sizes: '512x512',
-						type: 'image/png',
-						purpose: 'any maskable',
+						src: "/pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any maskable",
 					},
 				],
 			},
-		})],
+		}),
+	],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+		include: ["src/**/*.{test,spec}.{js,ts}"],
+	},
 });
